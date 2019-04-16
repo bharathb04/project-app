111b2ad8f37403a5c5fd82dd208a9f7c5151c6d3
bharathb04 <bharath@Bharaths-MacBook-Air.local>
2019-04-16 21:47:57 +1000

    Updates

---
+++ precache-manifest.8c2bfeb15b6834ba3be4bd1059dec20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "84d6895982450ca5b2f5",
+    "url": "/bharathb04/project-app/static/js/runtime~main.52e112fd.js"
+  },
+  {
+    "revision": "ea3580b62c82d51f5cc9",
+    "url": "/bharathb04/project-app/static/js/main.26652c56.chunk.js"
+  },
+  {
+    "revision": "b31f88599700091b52bd",
+    "url": "/bharathb04/project-app/static/js/2.ea1a7394.chunk.js"
+  },
+  {
+    "revision": "ea3580b62c82d51f5cc9",
+    "url": "/bharathb04/project-app/static/css/main.12a5ddf3.chunk.css"
+  },
+  {
+    "revision": "3d8d0afa4a23284b0e02e821f226cf25",
+    "url": "/bharathb04/project-app/index.html"
+  }
+];
